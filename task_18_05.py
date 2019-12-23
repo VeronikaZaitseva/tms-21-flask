0b8b874a5dab988e9b993a688ee1d46079652b7f
VeronikaZaitseva <nicusha1997@gmail.com>
2019-12-23 21:30:42 +0300

    two in power

--- task_18_05.py
+++ task_18_05.py
@@ -1,11 +1,14 @@
 # Создать сайт. При запросе по урлу /two_in/[number] возвращает 2 в степени number
 
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/two_in/<int:number>')
 def two_in(number):
-   return str(2**number)
+    return str(2 ** number)
+
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
