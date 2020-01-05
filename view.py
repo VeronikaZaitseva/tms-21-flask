134f7a09684a83d01c037eb22407e4a424f786fe
VeronikaZaitseva <nicusha1997@gmail.com>
2020-01-05 13:29:25 +0300

    hw18

--- view.py
+++ view.py
@@ -11,37 +11,11 @@ def link():
 @app.route("/products")
 def products():
     products = Product.query.all()
-    return render_template("templates/show_all.html", products=products)
-
-
-@app.route("/products/<id>")
-def product_info(id):
-    product = Product.query.filter(Product.id == id).first()
-    return render_template("product.html", product=product)
-
-
-@app.route("/products/<id>")
-def delete_product(id):
-    product = Product.query.filter(Product.id == id).first()
-    db.session.delete(product)
-    db.session.commit()
-    return "Saved"
-
-
-@app.route("/products/<id>", methods=["POST"])
-def edit_product(id):
-    if request.method == 'POST':
-        product = Product.query.filter(Product.id == id).first()
-        value_name = request.form["Value to edit"]
-        new_value = request.form["New value"]
-        setattr(product, value_name, new_value)
-        db.session.add(product)
-        db.session.commit()
-    return "Saved"
+    return render_template("show_all.html", products=products)
 
 
 @app.route("/products", methods=["POST"])
-def add_product():
+def add():
     if request.method == 'POST':
         name = request.form["Name"]
         price = request.form["Price"]
@@ -51,3 +25,31 @@ def add_product():
         db.session.add(product)
         db.session.commit()
     return products()
+
+
+@app.route("/products/<product_id>")
+def information(product_id):
+    product = Product.query.filter(Product.id == product_id).first()
+    return render_template("product.html", product=product)
+
+
+@app.route("/products/<product_id>")
+def delete(product_id):
+    product = Product.query.filter(Product.id == product_id).first()
+    db.session.delete(product)
+    db.session.commit()
+    return "Done"
+
+
+@app.route("/products/<product_id>", methods=["POST"])
+def edit(product_id):
+    if request.method == 'POST':
+        product = Product.query.filter(Product.id == product_id).first()
+        value_name = request.form["Value to edit"]
+        new_value = request.form["New value"]
+        setattr(product, value_name, new_value)
+        db.session.add(product)
+        db.session.commit()
+    return "Done"
+
+
